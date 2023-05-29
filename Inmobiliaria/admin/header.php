0afd67dfb19dc37a68113e4962b33a6af7579f27
ktevla <132966845+ktevla@users.noreply.github.com>
2023-05-29 11:02:54 -0500

    Delete Inmobiliaria directory

--- Inmobiliaria/admin/header.php
+++
@@ -1,4 +0,0 @@
-<header>
-    <h1>Administración de Inmobiliaria</h1>
-    <h2>Sistema de Administración Web Para Inmobiliaria</h2>
-</header>  
--- Inmobiliaria/contenido-footer.php
+++
@@ -1,2 +0,0 @@
-Inmobiliaria Web - Desarrollado por Código Web <br>
-<?php echo $config['telefono1'] . " - " . $config['email_contacto'] ?>
